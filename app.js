348eb9cb2e35cb3ae439c33f0618577ca8742280
Vivi Ya <viviviyanez@gmail.com>
2022-11-26 13:53:01 +0100

    server

--- app.js
+++ app.js
@@ -90,6 +90,14 @@ app.use("/api/auth", authRoutes);
 
 //ERROR HAnDLER
 app.use(errorController);
+
+//SERVE PAGES
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 //////////////////
 
 app.listen(port, "0.0.0.0", (err) => {
