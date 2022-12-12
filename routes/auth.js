0b4b6c508648542047847b1fa40897adf8671fb3
Vivi Ya <viviviyanez@gmail.com>
2022-12-12 16:05:36 +0100

    auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 const authorization = require("../middleware/authorization");
 
-router.get("/", authController.getUser);
+router.get("/user", authController.getUser);
 router.post("/register", authController.registerNewUser);
 router.post("/login", authController.loginUser);
 router.get("/logout", authController.logoutUser);
