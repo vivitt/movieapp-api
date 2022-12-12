6a99a57fd7665a6e3a1281697e217a74558551f3
Vivi Ya <viviviyanez@gmail.com>
2022-12-12 13:00:46 +0100

    authorization auth routes

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ const port = process.env.PORT || 3001;
 const cors = require("cors");
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
-
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/authController')
-const authorization = require("../middleware/authorization")
+const authController = require("../controllers/authController");
+const authorization = require("../middleware/authorization");
 
-router.get('/',  authController.getUser);
-router.post('/register',  authController.registerNewUser);
-router.post('/login', authController.loginUser);
-router.get('/logout', authController.logoutUser);
-
-
-
+router.get("/", authorization.checkAuthenticated.authController.getUser);
+router.post("/register", authController.registerNewUser);
+router.post("/login", authController.loginUser);
+router.get("/logout", authController.logoutUser);
 
 module.exports = router;
